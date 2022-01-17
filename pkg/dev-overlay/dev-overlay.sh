9133b75a06b542c7497ac09af6433e49279e60e9
Yuri Volchkov <yuri@zededa.com>
2022-01-17 16:47:28 +0000

    pivot from dev-overlay
    Signed-off-by: Yuri Volchkov <yuri@zededa.com>

--- pkg/dev-overlay/dev-overlay.sh
+++ pkg/dev-overlay/dev-overlay.sh
@@ -1,7 +1,47 @@
 #!/bin/sh
 
+do_pivot_root() {
+    if ! mount /dev/sda2 -t squashfs -o ro /mnt/ro-root; then
+        echo "Failed mounting original root at /mnt/ro-root"
+        return 1
+    fi
 
-echo "----- overlay init script ----"
+    if ! mount -t overlay -o \
+          lowerdir=/mnt/ro-root,workdir=/mnt/2nd-part/root-workdir,upperdir=/mnt/2nd-part/root-upper \
+          overlayfs-root /mnt/new-root; then
+        echo "Failed mounting overlayfs"
+        return 1
+    fi
+
+    cd /mnt/new-root/ || return 1
+    pivot_root . mnt
+
+    if ! mkdir -p /ro-root /2nd-part; then
+        echo "Failed creating new mount points"
+    fi
+
+    echo "Launching init in new root"
+    exec chroot . sh -c "$(cat <<END
+if ! mount --move /mnt/mnt/ro-root/ /ro-root; then
+   echo "Failed to move original root mountpoint (aka lower)"
+   /bin/sh
+fi
+
+if ! mount --move /mnt/mnt/2nd-part/ /2nd-part; then
+   echo "Failed to move 2nd partition mount point (contains upper)"
+   /bin/sh
+fi
+
+umount /mnt/mnt
+umount /mnt/proc
+umount /mnt/dev/
+umount /mnt
+
+exec /sbin/init
+END
+    )"
+
+}
 
 main() {
     # mount /proc
@@ -32,15 +72,14 @@ main() {
 	return 1
     fi
 
-    source /mnt/2nd-part/tools/pivot-root.sh
-    pivot_root
+    do_pivot_root
 }
 
 main "$@"
 
 cat << EOF
-Pivoting failed, launching shell. Note that this shell is now PID 1
-This allows you to call:
+Pivoting failed, launching shell to help investigate what went
+wrong. Note that this shell is now PID 1 This allows you to call:
 
    /sbin/init
 
